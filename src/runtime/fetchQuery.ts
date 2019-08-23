db9fd9aefda2993869181206035b5013d6bf767c
morrys <lorenzo.digiacomo85@gmail.com>
2019-08-23 14:01:56 +0200

    comment abous typing fetchQuery

--- src/runtime/fetchQuery.ts
+++ src/runtime/fetchQuery.ts
@@ -1,5 +1,10 @@
 import { fetchQuery as relayFetchQuery } from 'react-relay'
 
+/**
+ * https://github.com/facebook/relay/commit/c925281a609db199986bc9625d958f516642e95d#diff-391b381e9c777ee38b225619a38e1c1d
+ * For the moment the types are not explicit because it seems that in the next relay version there will be some important changes
+ */
+
 async function fetchQuery(...args) {
     const [environment] = args
 
