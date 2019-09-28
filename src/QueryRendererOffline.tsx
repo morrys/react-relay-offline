604794be66354bfe6ebab2cf54b2c2fd20f94deb
morrys <lorenzo.digiacomo85@gmail.com>
2019-09-28 17:46:13 +0200

    loadingComponent is optional also for react-native #20

--- src/QueryRendererOffline.tsx
+++ src/QueryRendererOffline.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import QueryRenderer, { Props } from './QueryRendererHook';
 import useRestore from './hooks/useRestore';
+import Loading from "./Loading";
 
 export interface OfflineProps extends Props {
   LoadingComponent?: any,
 };
 
 const QueryRendererOffline = (props: OfflineProps) => {
+  const { LoadingComponent = Loading } = props;
+  const rehydratate = useRestore(props.environment);
 
-  const rehydratate = useRestore(props.environment)
-
-  return rehydratate? <QueryRenderer {...props} /> : props.LoadingComponent ? props.LoadingComponent : <div>Loading...</div>;
+  return rehydratate ? <QueryRenderer {...props} /> : LoadingComponent;
 
 }
 
