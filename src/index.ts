12046e9c6b233370000aff43f5baafaa912ba11b
morrys <lorenzo.digiacomo85@gmail.com>
2021-01-09 17:39:46 +0100

    export relay-hooks, simplify migration

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,18 @@ export { Store, RecordSource } from '@wora/relay-store';
 export { NetInfo } from '@wora/netinfo';
 export { useNetInfo, useIsConnected } from '@wora/detect-network';
 export * from './RelayOfflineTypes';
+export {
+    NETWORK_ONLY,
+    STORE_THEN_NETWORK,
+    STORE_OR_NETWORK,
+    STORE_ONLY,
+    FetchPolicy,
+    useFragment,
+    useMutation,
+    usePagination,
+    useRefetchable,
+    useRefetchableFragment,
+    usePaginationFragment,
+    useSuspenseFragment,
+    RelayEnvironmentProvider,
+} from 'relay-hooks';
