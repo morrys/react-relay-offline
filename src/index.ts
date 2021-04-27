435c480585284d14d7fcca8463b1d309f7c92ce1
morrys <lorenzo.digiacomo85@gmail.com>
2021-04-27 12:07:17 +0200

    fix fetchQuery

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { loadQuery, loadLazyQuery } from './runtime/loadQuery';
 export { useQueryOffline as useQuery } from './hooks/useQueryOffline';
 export { usePreloadedQueryOffline as usePreloadedQuery } from './hooks/usePreloadedQueryOffline';
 export { useLazyLoadQueryOffline as useLazyLoadQuery } from './hooks/useLazyLoadQueryOffline';
-export { Environment, fetchQuery } from '@wora/relay-offline';
+export { Environment, fetchQuery_DEPRECATED } from '@wora/relay-offline';
 export { Store, RecordSource } from '@wora/relay-store';
 export { NetInfo } from '@wora/netinfo';
 export { useNetInfo, useIsConnected } from '@wora/detect-network';
