1f478ba2f5bb1322b732f2fda10658152bec8c02
morrys <lorenzo.digiacomo85@gmail.com>
2021-04-27 12:08:57 +0200

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { loadQuery, loadLazyQuery } from './runtime/loadQuery';
 export { useQueryOffline as useQuery } from './hooks/useQueryOffline';
 export { usePreloadedQueryOffline as usePreloadedQuery } from './hooks/usePreloadedQueryOffline';
 export { useLazyLoadQueryOffline as useLazyLoadQuery } from './hooks/useLazyLoadQueryOffline';
+// eslint-disable-next-line @typescript-eslint/camelcase
 export { Environment, fetchQuery_DEPRECATED } from '@wora/relay-offline';
 export { Store, RecordSource } from '@wora/relay-store';
 export { NetInfo } from '@wora/netinfo';
